ffd258bdb6c59b401abd3e0e5e303eb23530e28a
Chrys-Nicolaides <nicolaides.chrysanthe.com>
2020-09-10 08:33:20 -0700

    Standing times updated

--- src/components/MyTimers.js
+++ src/components/MyTimers.js
@@ -1,18 +1,18 @@
 const MyTimers = [
   {
-    name: "30 min", // 30 min stand : 1 hour sit
-    sit: 60 * 60,
-    stand: 0.5 * 60 * 60,
-  },
-  {
     name: "1 hour", // 1 hour stand : 1 hour sit
-    sit: 60 * 60,
     stand: 60 * 60,
+    sit: 60 * 60,
   },
   {
     name: "2 hour", // 2 hour stand : 1 hour sit
-    sit: 60 * 60,
     stand: 2 * 60 * 60,
+    sit: 60 * 60,
+  },
+  {
+    name: "3 hour", // 3 hour stand : 1 hour sit
+    stand: 3 * 60 * 60,
+    sit: 60 * 60,
   },
 ];
 
