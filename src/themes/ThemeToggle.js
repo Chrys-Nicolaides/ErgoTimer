3efc290aa0e3f6ab2c72243e2ba37dbb057ddd2f
Chrys-Nicolaides <nicolaides.chrysanthe.com>
2021-02-11 20:01:04 +0200

    Improved dark theme functionality

--- src/themes/ThemeToggle.js
+++ src/themes/ThemeToggle.js
@@ -1,94 +1,73 @@
 import React, { useState } from "react";
-import LightTheme from "./LightTheme";
-import DarkTheme from "./DarkTheme";
-import { ReactComponent as ThemeIconToggle } from "../images/ThemeIconToggle.svg";
+// import LightTheme from "./LightTheme";
+// import DarkTheme from "./DarkTheme";
+import { ReactComponent as SunToggleIcon } from "../images/SunToggleIcon.svg";
 import { ReactComponent as MoonToggleIcon } from "../images/MoonToggleIcon.svg";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-const ThemeToggle = () => {
-  const [themeColor, setThemeColor] = useState({
+const ThemeToggle = (props) => {
+  // const setTheme = { props };
+
+  const lightColors = {
     pink: "#e9acdb",
     blue: "#b0d7f5",
     purple: "#8f7cc2",
     text: "#374151",
     textLight: "#6b7280",
-  });
+  };
+  const darkColors = {
+    pink: "#B76FA7",
+    blue: "#5EA5DC",
+    purple: "#3B296E",
+    text: "#ffffff",
+    textLight: "#ffffff",
+  };
+
+  const [themeColor, setThemeColor] = useState(lightColors);
 
   let setColor = (object, color) =>
     document.documentElement.style.setProperty(`--${object}`, color);
 
-  let handleColourChange = (
-    pinkColor,
-    blueColor,
-    purpleColor,
-    textColor,
-    textLightColor
-  ) => {
-    setThemeColor({
-      pink: pinkColor,
-      blue: blueColor,
-      purple: purpleColor,
-      text: textColor,
-      textLight: textLightColor,
-    });
+  let handleColourChange = (input) => {
+    if (input === lightColors) {
+      props.setTheme("light-theme");
+    } else if (input === darkColors) {
+      props.setTheme("dark-theme");
+    }
+    setThemeColor(input);
+
+    setColor("theme-pink", input.pink);
+    setColor("theme-blue", input.blue);
+    setColor("theme-purple", input.purple);
+    setColor("theme-text", input.text);
+    setColor("theme-text-light", input.textLight);
   };
-  setColor("theme-pink", themeColor.pink);
-  setColor("theme-blue", themeColor.blue);
-  setColor("theme-purple", themeColor.purple);
-  setColor("theme-text", themeColor.text);
-  setColor("theme-text-light", themeColor.textLight);
 
   return (
     <Router>
       <div className="themes">
         <Switch>
           <Route path="/dark">
-            <div className="DarkTheme themeButton">
+            <div className="DarkTheme-themeButton">
               <Link to="/light" activeClassName="IsActive" exact={true}>
                 <MoonToggleIcon
                   alt="Theme Toggle"
-                  className="SunIconToggle"
-                  onClick={() =>
-                    handleColourChange(
-                      "#e9acdb",
-                      "#b0d7f5",
-                      "#8f7cc2",
-                      "#374151",
-                      "#6b7280"
-                    )
-                  }
-                />{" "}
-              </Link>
-            </div>
-          </Route>
-          <Route path="/">
-            <div className="LightTheme themeButton">
-              <Link to="/dark" activeClassName="IsActive" exact={true}>
-                <ThemeIconToggle
-                  alt="Theme Toggle"
-                  className="MoonIconToggle"
-                  onClick={() =>
-                    handleColourChange(
-                      "#B76FA7",
-                      "#5EA5DC",
-                      "#3B296E",
-                      "white",
-                      "white"
-                    )
-                  }
+                  className="MoonToggleIcon"
+                  onClick={() => handleColourChange(lightColors)}
                 />
               </Link>
             </div>
           </Route>
-        </Switch>
-      </div>
-      <div className="viewport">
-        <Switch>
-          <Route path="/dark">
-            <DarkTheme />
-          </Route>
           <Route path="/light">
-            <LightTheme />
+            <div className="LightTheme-themeButton">
+              <Link to="/dark" activeClassName="IsActive" exact={true}>
+                <SunToggleIcon
+                  alt="Theme Toggle"
+                  className="SunToggleIcon"
+                  onClick={() => handleColourChange(darkColors)}
+                />
+              </Link>
+            </div>
           </Route>
         </Switch>
       </div>
